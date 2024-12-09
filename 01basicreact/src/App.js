4d2be70323308f15b85155faf4eb30d06f741b62
Tabish <tabishraj7@gmail.com>
2024-12-09 11:07:27 +0530

    create project with help of  react and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Learn React | Raj</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+    <h1>Learn React with Vite | Raj</h1>
+  )
+}
+
+export default App
