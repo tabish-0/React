ddaae8c142c130883da5a1a6ab60484a28a54e69
Tabish <tabishraj7@gmail.com>
2024-12-10 12:13:31 +0530

    tailwind props and counter

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myobj = {
+    uername: "Raj",
+    age: 19
+  }
+
+  let newArr = [1,2,3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="raj" btnText="Click me"/>
+      <Card username="tabish" />
+    </>
+  )
+}
+
+export default App
